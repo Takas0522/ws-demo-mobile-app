317ec5e72ea8ec7434c59f9bc29b0bed575a9575
Takas0522 <t_o_19880522@outlook.com>
2026-03-01 12:07:07 +0900

    ユニットテストの構成追加

---
+++ src/windows-app/WsDemoMobileApp.WindowsApp.Test/WsDemoMobileApp.WindowsApp.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace WsDemoMobileAppWindowsAppTest
+{
+	TEST_CLASS(WsDemoMobileAppWindowsAppTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
